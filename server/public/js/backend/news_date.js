84e4276862dbdf071d775aab6f1716210b65006e
ZeroOneZero29 <zhigadlov29@gmail.com>
2025-01-06 23:30:03 +0700

    fix check border

--- server/public/js/backend/news_date.js
+++ server/public/js/backend/news_date.js
@@ -3,7 +3,12 @@ const axios = require("axios");
 let config = {
   method: "get",
   maxBodyLength: Infinity,
-  url: "https://newsdata.io/api/1/latest?apikey=pub_643889e79a1ddd7adf94cfc11c698648ef361&category=technology&language=ru",
+
+  url: "https://newsdata.io/api/1/latest?apikey=pub_6457648ac306c39e1085e4bb853f85fdb2718&category=technology&language=ru",
+  //url: "https://newsdata.io/api/1/latest?apikey=pub_6457750337719d4e54a3f552ccad4d7ebbd91&category=technology&language=ru",
+
+  //url: "https://newsdata.io/api/1/latest?apikey=pub_641468c33553d5cb7320585d226ee93b9af79&category=technology&language=ru",
+
   headers: {},
 };
 
